05635cc988eb42e895765ae2f05178d1e016be47
Diogo Schoninger <schoninger.diogo@gmail.com>
2022-03-12 16:53:29 -0300

    Funcionando

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Calculadora de ICMS-ST
  Script utilizado para calcular ICMS-ST de notas fiscais
+
+ O cálculo se dá da seguinte forma:
+ * ICMS Próprio = Valor total do produto X Alíquota interna
+ * ICMS-ST = (Valor total do produto + MVA original (%)) X Alíquota interna
+ * Valor a recolher = ICMS-ST - ICMS Próprio
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const divRoot = document.getElementById('root');
 var numProd = 1;
 
-function createProduct() {
+function createProductForm() {
   var html = document.createElement('div');
   html.innerHTML = `
     <h2>Produto ${numProd}</h2>
@@ -25,5 +25,44 @@ function createProduct() {
   return html;
 }
 
-function addProduct() {
+function addProductForm() {
+  if (numProd === 1) divRoot.innerHTML = '';
+  divRoot.appendChild(createProductForm());
+}
+
+function calculate() {
+  var icmsProprio = 0;
+  var icmsST = 0;
+  var valorTotalProdutos = 0;
+
+  for (let i = 1; i < numProd; i++) {
+    let valueProduct = document.getElementById(`valueProduct${i}`).value;
+    let aliqInt = document.getElementById(`aliqInt${i}`).value;
+    let mva = document.getElementById(`mva${i}`).value;
+
+    valueProduct = Number.parseFloat(valueProduct.replace(',', '.'));
+    aliqInt = Number.parseFloat(aliqInt.replace(',', '.'));
+    mva = Number.parseFloat(mva.replace(',', '.'));
+    
+    console.log(valueProduct);
+    console.log(aliqInt);
+    console.log(mva);
+
+    valorTotalProdutos += valueProduct;
+    icmsProprio += valueProduct * (aliqInt / 100);
+    icmsST += (valueProduct + (valueProduct * (mva / 100))) * (aliqInt / 100);
+  }
+
+  var icmsARecolher = icmsST - icmsProprio;
+
+  numProd = 1;
+
+  divRoot.innerHTML = `
+    <ul>
+      <li>Valor total dos produtos: ${valorTotalProdutos.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</li>
+      <li>Valor ICMS-ST: ${icmsST.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</li>
+      <li>Valor ICMS Próprio: ${icmsProprio.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</li>
+      <li>Valor ICMS a recolher: ${icmsARecolher.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</li>
+    </ul>
+  `;
 }
